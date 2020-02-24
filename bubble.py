2c3372655a9adc9cfb10d4cc8e788e30bbd46a59
човаще <55867948+vkate174@users.noreply.github.com>
2020-02-24 21:09:37 +0500

    Add files via upload

---
+++ bubble.py
@@ -0,0 +1,12 @@
+#bubble
+def bubble_sort(nums):
+    swapped = True
+    while swapped:
+        swapped = False
+        for i in range(len(nums) - 1):
+            if nums[i] > nums[i + 1]:
+                nums[i], nums[i+1] = nums[i+1], nums[i]
+                swapped = True
+random = [-5,20,19,8,0]
+bubble_sort(random)
+print(random)
