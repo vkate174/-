93ade3061164a87af9c861aa1c15e31474592088
човаще <55867948+vkate174@users.noreply.github.com>
2020-04-14 14:49:54 +0500

    Update bubble.py

--- bubble.py
+++ bubble.py
@@ -1,11 +1,11 @@
 #bubble
 def bubble_sort(nums):
-swapped = True
-while swapped:
-swapped = False
-for i in range(len(nums) - 1):
-if nums[i] > nums[i + 1]:
-nums[i], nums[i+1] = nums[i+1], nums[i]
+ swapped = True
+ while swapped:
+  swapped = False
+  for i in range(len(nums) - 1):
+   if nums[i] > nums[i + 1]:
+     nums[i], nums[i+1] = nums[i+1], nums[i]
 swapped = True
 import random
 arry = [random.randint(0, 2600) for i in range(2600)]
